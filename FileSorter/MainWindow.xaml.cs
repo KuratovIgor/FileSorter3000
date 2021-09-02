a869aa2eb6fb4797a23ea34c096ceb034368b0ad
KuratovIgor <kuratovigor084@gmail.com>
2021-09-03 00:09:41 +0700

    Старт разработки интерфейса

---
+++ FileSorter/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace FileSorter
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+     //   public DriveInfo[] DriveCollection = DriveInfo.GetDrives();
+        public ObservableCollection<DriveInfo> DriveCollection { get; set; } = new ObservableCollection<DriveInfo> { };
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                DriveCollection.Add(drive);
+            }
+        }
+    }
+}
